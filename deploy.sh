e02ed5200a0b8e3959f5c7d1641a83241761c36d
Kirk Hansen <hanski07@luther.edu>
2018-02-20 08:36:01 -0600

    Move updating the stack to the end
    This woud be better in a catch output scenario, however...

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,12 @@ then
     # make sure we have the latest python code built, and npm code for lambda is up to date
     ./build.sh
 
-    # see if we have made changes to cloudformation and attempt to run them.
-    aws --profile=${AWS_PROFILE} cloudformation update-stack --stack-name svo-print \
-        --template-body "file://${DIR}/svo-print.template" --capabilities CAPABILITY_IAM
-
     # Update the lambda code
     aws --profile=${AWS_PROFILE} s3 cp "${DIR}/html-to-pdf/html-to-pdf.zip" s3://svo-print-config/lambda_code/
     aws --profile=${AWS_PROFILE} lambda update-function-code --region=us-east-1 \
         --function-name=html-to-pdf --s3-bucket=svo-print --s3-key=lambda_code/html-to-pdf.zip
+
+    # see if we have made changes to cloudformation and attempt to run them.
+    aws --profile=${AWS_PROFILE} cloudformation update-stack --stack-name svo-print \
+        --template-body "file://${DIR}/svo-print.template" --capabilities CAPABILITY_IAM
 fi
