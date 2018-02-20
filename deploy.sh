3394e24639604dc4005c25b76c540cd955b0c65b
Kirk Hansen <hanski07@luther.edu>
2018-02-20 08:33:11 -0600

    Fixed CF, and added all resources to it

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 AWS_PROFILE=${1:-svo}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 read -p "You're about to deploy to production with the profile ${AWS_PROFILE}! Continue? [n\Y] "
 echo
@@ -8,6 +11,13 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]
 then
     # make sure we have the latest python code built, and npm code for lambda is up to date
     ./build.sh
-    aws --profile=${AWS_PROFILE} s3 cp html-to-pdf/html-to-pdf.zip s3://svo-print/lambda_code/
-    aws --profile=${AWS_PROFILE} lambda update-function-code --region=us-east-1 --function-name=html-to-pdf --s3-bucket=svo-print --s3-key=lambda_code/html-to-pdf.zip
+
+    # see if we have made changes to cloudformation and attempt to run them.
+    aws --profile=${AWS_PROFILE} cloudformation update-stack --stack-name svo-print \
+        --template-body "file://${DIR}/svo-print.template" --capabilities CAPABILITY_IAM
+
+    # Update the lambda code
+    aws --profile=${AWS_PROFILE} s3 cp "${DIR}/html-to-pdf/html-to-pdf.zip" s3://svo-print-config/lambda_code/
+    aws --profile=${AWS_PROFILE} lambda update-function-code --region=us-east-1 \
+        --function-name=html-to-pdf --s3-bucket=svo-print --s3-key=lambda_code/html-to-pdf.zip
 fi
